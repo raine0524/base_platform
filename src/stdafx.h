42ce38c66a6c4d3fcdca8ef345579aef0d8aa93b
daniel <1158359135@qq.com>
2018-05-22 17:57:34 +0800

    control_cmd 移入stdafx.h 预编译头，移除相应头文件

--- src/stdafx.h
+++ src/stdafx.h
@@ -4,12 +4,21 @@
 
 #include "../include/crx_pch.h"
 
+enum CTL_CMD {
+    CMD_REG_NAME = 0,       //名字注册
+    CMD_SVR_ONLINE,         //服务上线
+    CMD_HELLO,              //握手
+    CMD_GOODBYE,            //挥手
+    CMD_CONN_CON,           //连接建立
+    CMD_CONN_DES,           //连接断开
+    CMD_MAX_VAL,
+};
+
 //////////////////////////////////////////////////////////////////////////
 //kbase library (self custome implementation)
 //msgpack
 #include "../include/msgpack.hpp"
 
-#include "control_cmd.h"
 #include "net_base.h"
 #include "pywrap_impl.h"
 #include "sched_impl.h"
